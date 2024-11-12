2a68c84a0783dbe8d6c49990edcab7ae31f39534
learngithubz <perjalananswift@gmail.com>
2024-11-12 16:14:39 +0700

    Initial Commit

---
+++ PYB Photo Effect/PYB_Photo_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PYB_Photo_EffectApp.swift
+//  PYB Photo Effect
+//
+//  Created by Your Future on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PYB_Photo_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
